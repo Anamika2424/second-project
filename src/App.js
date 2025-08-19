8d2fc79e5e23c5e3e1b7b12dee45f7e57bb66c38
Anamika2424 <kuteanamika7@gmail.com>
2025-08-19 22:26:37 +0530

    odd even program

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+
 import './App.css';
 
-function App(){
-let array= [2 , 4, 5, 6 , 7];
+function App() {
+let array = [2 ,3,4,5,6,7];
 
-  return (
-<div>{
-    array.map((singleElement ) =>{
-      return  singleElement*2
+    return (
+        <div>
+       { array.map((singleElement) =>{
+            if(singleElement %2===0){
+             return <div><i>{singleElement}</i></div>
+            }else{
+             return <div> <strong>{singleElement}</strong></div>
+            }
+        }
+    )
+    
+    
     }
-  )
-}
-   </div> 
-  );
-  
-}
+    </div>
+    );
+}``
 export default App;
+
+
+ 
