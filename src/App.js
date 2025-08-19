5ca425364e651c15be3bc392cbe8406f0d560517
Anamika2424 <kuteanamika7@gmail.com>
2025-08-19 16:57:50 +0530

    multiplication project

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import './App.css';
 
- function Calculator ()  {
-let operator = "+";
-let a= 10, b=5, result= 0;
-
-function Addition (){
-result = a +b;
-console.log("Addition is : " , result );
-}
-function Substraction(){
-result = a -b;
-console.log("Substraction is : " , result );
-
-}
-function Multiplication (){
-result = a *b;
-console.log("Muliplication is : " , result );
-}
-function Division(){
-result = a /b;
-console.log("Division is : " , result );
-}
+function App(){
+let array= [2 , 4, 5, 6 , 7];
 
   return (
-   <div>
-    <h2>Calculator</h2>
-    <div>First number is :10</div>
-    <div>Second number is :5</div>
-    
-  <div>  <button onClick={Addition}>Addition</button></div>
-  <div>  <button onClick={Substraction}>Substraction</button></div> 
-   <div> <button onClick={Multiplication}>Muliplication</button></div>
-  <div> <button onClick={Division}>Division</button></div>
-
-
-   </div>
-  );
+<div>{
+    array.map((singleElement ) =>{
+      return  singleElement*2
+    }
+  )
 }
-
-export default Calculator;
+   </div> 
+  );
+  
+}
+export default App;
