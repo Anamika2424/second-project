ec9d1e3328d135689683287d85965642040e711d
Anamika2424 <kuteanamika7@gmail.com>
2025-08-21 15:18:59 +0530

    tables using map ()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-
 import './App.css';
 
 function App() {
-let array = [2 ,3,4,5,6,7];
-
+    let array = [2, 4, 6, 7, 8, 9];
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
         <div>
-       { array.map((singleElement) =>{
-            return <div>{singleElement*2}</div>
-        }
+
+            {array.map((singleElement) => {
+                return <div>
+                    {
+                        numbers.map((sequenceOfNumbers) => {
+                            return <span> {singleElement * sequenceOfNumbers}</span>
+                        }
+                        )
+                    }
+                </div>
+            })
+            }
+        </div>
+
     )
-    
-    
-    }
-    </div>
-    );
 }
 export default App;
-
-
- 
