2f99d941798b78291bb7a0237233489451c1b39c
Anamika2424 <kuteanamika7@gmail.com>
2025-08-25 15:18:44 +0530

    displaying value on a button click

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
+
 import React, { useState } from 'react'
 
 function App() {
-    const [age,setAge] = useState();
-function handleAgeChange(event){
-    setAge(event.target.value)
-}
-    return  (
-        <div>
-             <input name="age" value={age} onChange={handleAgeChange}></input>
-            <div>{age}</div>
-        </div>
+    const [age, setAge] = useState();
+    const [showBlock, setShowBlock] = useState(false);
 
+    function handleAgeChange(event) {
+        setAge(event.target.value);
+    }
+
+    function onDisplay() {
+        setShowBlock(true);
+    }
+
+    return (
+        <div>
+            <input
+                name="age"
+                value={age}
+                onChange={handleAgeChange}
+            />
+
+            <button onClick={onDisplay} >Display Value</button>
+
+            <div> {showBlock ?
+                <div>{age}</div>:
+                <div>please press the button</div>
+            }</div>
+        </div>
     )
 }
+
 export default App;
