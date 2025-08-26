495b874d8a97b76c96bfadd0672de3e0fea49a8d
Anamika2424 <kuteanamika7@gmail.com>
2025-08-26 13:41:10 +0530

    addition using parseInt ()

--- src/App.js
+++ src/App.js
@@ -3,33 +3,29 @@ import './App.css';
 import React, { useState } from 'react'
 
 function App() {
-    const [age, setAge] = useState();
-    const [showBlock, setShowBlock] = useState(false);
+    const [  num1 ,setNum1] =useState();
+    const [num2 ,setNum2] =useState();
+    const [sum ,setSum] =useState();
 
-    function handleAgeChange(event) {
-        setAge(event.target.value);
+    function handleAddition(){
+      setSum (num1 +num2);
     }
-
-    function onDisplay() {
-        setShowBlock(true);
-    }
-
+   function handleNum1Change(event){
+      setNum1  (parseInt(event.target.value));
+   }
+    function handleNum2Change(event){
+     setNum2   (parseInt(event.target.value));
+   }
+    
     return (
+
         <div>
-            <input
-                name="age"
-                value={age}
-                onChange={handleAgeChange}
-            />
-
-            <button onClick={onDisplay} >Display Value</button>
-
-            <div> {showBlock ?
-                <div>{age}</div>:
-                <div>please press the button</div>
-            }</div>
+            <input name="number1" value={num1} onChange={handleNum1Change }  />
+            <input name="number2" value={num2} onChange={handleNum2Change }  />
+            <button onClick={handleAddition}>Submit</button>
+            <div>{sum}</div>
         </div>
     )
-}
 
+}
 export default App;
