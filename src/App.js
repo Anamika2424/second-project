05e1e1e6e9c2c5aa1c652dee87b7ba1ff20314b6
Anamika2424 <kuteanamika7@gmail.com>
2025-08-26 14:33:05 +0530

    simple calculator in react

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 import React, { useState } from 'react'
 
 function App() {
-    const [  num1 ,setNum1] =useState();
+    const [ num1 ,setNum1] =useState();
     const [num2 ,setNum2] =useState();
-    const [sum ,setSum] =useState();
+    const [sum ,setSum]=useState();
+    const [substract,setSubstarct] =useState()
+    const [multiply ,setMultiply]=useState();
+    const [division ,setDivision]=useState();
 
-    function handleAddition(){
-      setSum (num1 +num2);
+    function handleCalculator(){
+      setSum (num1 + num2); 
+      setSubstarct (num1 - num2); 
+      setMultiply (num1 * num2); 
+      setDivision(num1 / num2);     
+        
     }
+     
    function handleNum1Change(event){
-      setNum1  (parseInt(event.target.value));
+      setNum1(parseInt(event.target.value));
    }
     function handleNum2Change(event){
-     setNum2   (parseInt(event.target.value));
+     setNum2(parseInt(event.target.value));
    }
     
     return (
@@ -22,8 +30,13 @@ function App() {
         <div>
             <input name="number1" value={num1} onChange={handleNum1Change }  />
             <input name="number2" value={num2} onChange={handleNum2Change }  />
-            <button onClick={handleAddition}>Submit</button>
+            <button onClick={handleCalculator}>Submit</button>
+
+
             <div>{sum}</div>
+            <div>{substract}</div>
+            <div>{multiply}</div>
+            <div>{division}</div>
         </div>
     )
 
