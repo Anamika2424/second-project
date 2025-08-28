cd9cd654e9463bc7f447c46a0789dda58a517380
Anamika2424 <kuteanamika7@gmail.com>
2025-08-28 16:12:03 +0530

    calculation operations on four different buttons

--- src/App.js
+++ src/App.js
@@ -1,44 +1,93 @@
 import './App.css';
-
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function App() {
-    const [ num1 ,setNum1] =useState();
-    const [num2 ,setNum2] =useState();
-    const [sum ,setSum]=useState();
-    const [substract,setSubstarct] =useState()
-    const [multiply ,setMultiply]=useState();
-    const [division ,setDivision]=useState();
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [additionresult, setAdditionResult] = useState("");
+  const [substractionresult, setSubstractionResult] = useState("");
+  const [multiplicationresult, setMultiplicationResult] = useState("");
+  const [divisionresult, setDivisionResult] = useState("");
 
-    function handleCalculator(){
-      setSum (num1 + num2); 
-      setSubstarct (num1 - num2); 
-      setMultiply (num1 * num2); 
-      setDivision(num1 / num2);     
-        
-    }
-     
-   function handleNum1Change(event){
-      setNum1(parseInt(event.target.value));
-   }
-    function handleNum2Change(event){
-     setNum2(parseInt(event.target.value));
-   }
+
+  
+  
+
+  function handleAddition() {
+    setAdditionResult(num1 + num2)
+    setMultiplicationResult("")
+    setDivisionResult("")
+    setSubstractionResult("")
+  }
+
+  function handleSubstract() { 
+    setSubstractionResult(num1 - num2)
+    setAdditionResult("")
+    setMultiplicationResult("")
+    setDivisionResult("")
+   
+  }
+
+  function handleMultiply() {
+    setAdditionResult("")
+    setSubstractionResult("")
     
-    return (
+    setMultiplicationResult(num1 * num2)
+    setDivisionResult("")
 
-        <div>
-            <input name="number1" value={num1} onChange={handleNum1Change }  />
-            <input name="number2" value={num2} onChange={handleNum2Change }  />
-            <button onClick={handleCalculator}>Submit</button>
+    
+  }
+
+  function handleDivision() {
+   setAdditionResult("")
+    setSubstractionResult("")
+    setDivisionResult(num1 / num2);
+    setMultiplicationResult("")
+
+    
+  }
+
+  function handleNum1Change(event) {
+    setNum1(parseInt(event.target.value));
+  }
+
+  function handleNum2Change(event) {
+    setNum2(parseInt(event.target.value));
+  }
+
+  return (
+    <div>
+      <input
+        name="number1"
+        value={num1}
+        onChange={handleNum1Change}
+      />
+      <input
+        name="number2"
+        value={num2}
+        onChange={handleNum2Change}
+      />
+
+      <span><button onClick={handleAddition}>Addition</button></span>
+      <span><button onClick={handleSubstract}>Substraction</button></span>
+      <span><button onClick={handleMultiply}>Multiplication</button></span>
+      <span><button onClick={handleDivision}>Division</button></span>
 
 
-            <div>{sum}</div>
-            <div>{substract}</div>
-            <div>{multiply}</div>
-            <div>{division}</div>
-        </div>
-    )
-
+{additionresult !== "" && (
+        <h1>Addition of {num1}+ {num2}: {additionresult}</h1>
+      )}
+      {substractionresult !== "" && (
+        <h1>Subtraction of {num1}- {num2}: {substractionresult}</h1>
+      )}
+      {multiplicationresult !== "" && (
+        <h1>Multiplication of {num1}* {num2}: {multiplicationresult}</h1>
+      )}
+      {divisionresult !== "" && (
+        <h1>Division of {num1}/ {num2}: {divisionresult}</h1>
+      )}
+    </div>
+  );
 }
+
 export default App;
