94f60213419ff6a2c89c43b35b0a0c2363d86f78
Anamika2424 <kuteanamika7@gmail.com>
2025-09-01 11:27:49 +0530

    highlight age in the table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import React, { useState } from 'react'
 
+
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  
-  const [student,setStudent]= useState([
-    { id :1,name:"Anamika",age:22,gaon:"Pimpari pendhar"},
-    {id :2,name:"Nikita",age:23,gaon:"Rajuri"},
-    {id:3,  name :"Apeksha" ,age:24,gaon:"Ale"}
-
+  const [student, setStudent] = useState([
+    { no: 1, name: "Anamika", age: 22, gaon: "Pimpari pendhar" },
+    { no: 2, name: "Nikita", age: 23, gaon: "Rajuri" },
+    { no: 3, name: "Apeksha", age: 24, gaon: "Ale" },
+    { no: 4, name: "Trusha", age: 67, gaon: "Alephata" },
+    { no: 5, name: "Sharvani", age: 56, gaon: "Belhe" },
   ]);
+
   return (
     <div>
-      < table border={1}>
+      <table border={1}>
         <thead>
           <tr>
             <th>No</th>
@@ -22,19 +24,19 @@ function App() {
           </tr>
         </thead>
         <tbody>
-    {student.map((student)=>(
-      <tr key={student.id}>
-        <td>{student.id}</td>
-        <td>{student.name}</td>
-        <td >{student.age}</td>
-        <td>{student.gaon}</td>
-      
-      </tr>
-
-    )
-  )}
-  </tbody>
-  </table>
+          {student.map((student) => (
+            <tr 
+              key={student.no} 
+              style={ student.age > 50 ? { backgroundColor: "black", color: "white" } : {} }
+            >
+              <td>{student.no}</td>
+              <td>{student.name}</td>
+              <td>{student.age}</td>
+              <td>{student.gaon}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
